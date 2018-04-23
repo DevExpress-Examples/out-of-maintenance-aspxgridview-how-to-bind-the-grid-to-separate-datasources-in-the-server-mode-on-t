54f251dd1e717f43c6016fdcc476e88489f9378e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/App_Code/XPOHelper.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Xpo.DB.Cte;
+using System.Xml;
+
+    public class XPOHelper
+    {
+        UnitOfWork uow;
+        IDataLayer dal;
+        String strCTEName = "";
+        System.Web.UI.Page page;
+        public XPServerCollectionSource GetXCS(string sServerName, string sUserName, string sPassword, string sDBName, string sSql, string sColumnXML,
+            System.Web.UI.Page oPage, bool p_bHasKeyField)
+        {
+            XPServerCollectionSource l_xcs = null;
+            page = oPage;
+            page.Unload += new EventHandler(Page_Unload);
+            strCTEName = "CTE_" + DateTime.Now.Hour.ToString() + DateTime.Now.Minute.ToString() + DateTime.Now.Millisecond.ToString();
+            //Use your own GetConnectionString method based on the passed parameters
+            string connectionString = MSSqlConnectionProviderWithCte.GetConnectionString(sServerName, sDBName);
+            XPDictionary dict = new ReflectionDictionary();
+            XPClassInfo classInfo = dict.CreateClass(dict.GetClassInfo(typeof(BasePersistentClass)), strCTEName);
+
+            XmlDocument l_xmlDoc = new XmlDocument();
+            l_xmlDoc.LoadXml(sColumnXML);
+            XmlNodeList l_xnl = l_xmlDoc.SelectNodes("//FLD");
+            for (int i = 0; i < l_xnl.Count; i++)
+            {
+                Type l_type = null;
+                switch (l_xnl[i]["DATTYP"].InnerText.ToLower())
+                {
+                    case "int64":
+                        l_type = typeof(Int64);
+                        break;
+                    case "byte[]":
+                        l_type = typeof(Byte[]);
+                        break;
+                    case "boolean":
+                        l_type = typeof(Boolean);
+                        break;
+                    case "string":
+                        l_type = typeof(String);
+                        break;
+                    case "char[]":
+                        l_type = typeof(Char[]);
+                        break;
+                    case "datetime":
+                        l_type = typeof(DateTime);
+                        break;
+                    case "datetimeoffset":
+                        l_type = typeof(DateTimeOffset);
+                        break;
+                    case "decimal":
+                        l_type = typeof(Decimal);
+                        break;
+                    case "double":
+                        l_type = typeof(Double);
+                        break;
+                    case "int32":
+                        l_type = typeof(Int32);
+                        break;
+                    case "single":
+                        l_type = typeof(Single);
+                        break;
+                    case "int16":
+                        l_type = typeof(Int16);
+                        break;
+                    case "object":
+                        l_type = typeof(Object);
+                        break;
+                    case "timeSpan":
+                        l_type = typeof(TimeSpan);
+                        break;
+                    case "byte":
+                        l_type = typeof(Byte);
+                        break;
+                    case "guid":
+                        l_type = typeof(Guid);
+                        break;
+                    case "xml":
+                        l_type = typeof(XmlDocument);
+                        break;
+                }
+                
+                if (i == 0)
+                    classInfo.CreateMember(l_xnl[i]["COL"].InnerText, l_type, new KeyAttribute(true));
+                else
+                    classInfo.CreateMember(l_xnl[i]["COL"].InnerText, l_type);
+            }
+
+            dal = new SimpleDataLayer(dict, XpoDefault.GetConnectionProvider(connectionString, AutoCreateOption.SchemaAlreadyExists));
+            uow = new UnitOfWork(dal);
+            string l_strQuery = "";
+            l_strQuery = " AS (" + sSql + ")";
+            uow.RegisterCte(strCTEName, @l_strQuery);
+            try
+            {
+
+                l_xcs = new XPServerCollectionSource(uow, classInfo);
+                return l_xcs;
+            }
+            finally
+            {
+
+            }
+            return l_xcs;
+        }
+
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+            uow.UnregisterCte(strCTEName);
+            uow.Dispose();
+            dal.Dispose();
+            page.Unload -= new EventHandler(Page_Unload);
+        }
+
+        [NonPersistent]
+        public class BasePersistentClass : XPLiteObject
+        {
+            public BasePersistentClass(Session session) : base(session) { }
+            public BasePersistentClass(Session session, XPClassInfo classInfo) : base(session, classInfo) { }
+        }
+    }
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,63 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Data.OleDb
+Imports System.Data
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+		If Session("currentSource") IsNot Nothing Then
+			Select Case Session("currentSource").ToString()
+				Case "Datasource1"
+					BindToDS1()
+				Case "Datasource2"
+					BindToDS2()
+			End Select
+		End If
+	End Sub
+	Private Function GetFieldInfoFromQuery(ByVal p_strQuery As String, ByVal p_strConnectionString As String) As String
+		Dim l_adapterSchema As New OleDbDataAdapter(p_strQuery, p_strConnectionString)
+		Dim g_dtSchema As New DataTable()
+		l_adapterSchema.FillSchema(g_dtSchema, SchemaType.Source)
+		Dim l_strColumnSchema As String = "<FLDINF>"
+		For Each l_dcolum As DataColumn In g_dtSchema.Columns
+			l_strColumnSchema = String.Concat(l_strColumnSchema, "<FLD>")
+			l_strColumnSchema = String.Concat(l_strColumnSchema, "<COL>", l_dcolum.ColumnName, "</COL>")
+			l_strColumnSchema = String.Concat(l_strColumnSchema, "<DATTYP>", l_dcolum.DataType.ToString().ToLower().Replace("system.", ""), "</DATTYP>")
+			l_strColumnSchema = String.Concat(l_strColumnSchema, "</FLD>")
+		Next l_dcolum
+		l_strColumnSchema &= "</FLDINF>"
+		Return l_strColumnSchema
+	End Function
+
+	Private Sub BindToDS1()
+		BindGrid("SELECT * FROM [TABLE]", "DATABASE_NAME")
+	End Sub
+	Private Sub BindToDS2()
+		BindGrid("SELECT * FROM [TABLE]", "DATABASE2_NAME")
+	End Sub
+	Protected Sub ASPxGridView1_CustomCallback(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxGridView.ASPxGridViewCustomCallbackEventArgs)
+		If e.Parameters = "1" Then
+			Session("currentSource") = "Datasource1"
+			BindToDS1()
+		End If
+		If e.Parameters = "2" Then
+			Session("currentSource") = "Datasource2"
+			BindToDS2()
+		End If
+	End Sub
+	Private Sub BindGrid(ByVal sqlQuery As String, ByVal DBName As String)
+		Dim connectionString As String = String.Format("Provider=SQLOLEDB;Server=servername;Initial Catalog = {0};Integrated security = SSPI;", DBName)
+		'configure your connection string
+
+		Dim helper As New XPOHelper()
+		Dim columnSchema As String = GetFieldInfoFromQuery(sqlQuery, connectionString)
+		Dim collection As DevExpress.Xpo.XPServerCollectionSource = helper.GetXCS("servername", "username", "password", DBName, sqlQuery, columnSchema, Me.Page, True)
+		ASPxGridView1.Columns.Clear()
+		ASPxGridView1.AutoGenerateColumns = True
+		ASPxGridView1.DataSource = collection
+		ASPxGridView1.DataBind()
+	End Sub
+End Class
